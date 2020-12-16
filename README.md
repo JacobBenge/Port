5eb60c3a4b8bcb85faf8399c9b1613c1d5684e15
JacobBenge <benge.jacob@gmail.com>
2020-12-16 12:08:22 -0700

    Section14 of course completed
    Contains notes from lecture

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UdemyPractice
- A demonstration of web development and use of git
+ A demonstration of web development and use of git.
+
+The majority of this repository will be mimicking this Udemy Course:
+https://www.udemy.com/course/the-web-developer-bootcamp/
---
+++ Section14/JavascriptNotes.js
@@ -0,0 +1,30 @@
+// Javascript notes
+
+//OPERATORS
+
+	// clear the console
+	clear()
+	// returns the data type of the input. ex. typeof 4 = "number". ex. typeof 0/0 = NaN
+	typeof
+
+// STORING VARIABLES
+
+	//basic syntax for variable. The var keyword is the old way. Cannot start variable name with a number. Use camelCase.
+	let year = 1985;
+	var year = 1985;
+	
+	// boolean logic. Using "is" in boolean name helps show its a variable. Ex.  isGameOver 
+	true
+	false
+	let isActive = true;
+	
+	// basic syntax for constant. Great for conversions.
+	const pi = 3.14159;
+	
+	//incremetation and decrementation
+	score += 10;
+	score *= 10;
+	score -= 10;
+	score /= 10;
+	score--;
+	score++;
