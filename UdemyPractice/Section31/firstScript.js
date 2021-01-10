594b4ab13e83a2d700cf3e3f0ab75d770401179e
JacobBenge <benge.jacob@gmail.com>
2021-01-10 16:41:05 -0700

    After file corruption fix

--- UdemyPractice/Section31/firstScript.js
+++ UdemyPractice/Section31/firstScript.js
@@ -1,3 +1,8 @@
 for(let i= 0; i< 10; i++){
     console.log('hello from first script');
 }
+
+const args = process.argv.slice(2);
+for (let arg of args) {
+    console.log(`These are arguments that are passed in from the command line following 'node firstScript.js : ${arg}`)
+}
