48ecf3c5388461e9df0395f43bb35487414ad2f0
JacobBenge <benge.jacob@gmail.com>
2021-01-10 18:06:53 -0700

    Section32 complete

--- UdemyPractice/Section32/LangGuessr/index.js
+++ UdemyPractice/Section32/LangGuessr/index.js
@@ -1,14 +1,17 @@
 // REMEMBER TO RUN "npm install" in your terminal (in the same directory as this file)
+// example command: node index.js 'Alle menslike wesens word vry'
+// https://github.com/wooorm/franc
 const franc = require("franc");
+// https://github.com/adlawson/nodejs-langs
 const langs = require("langs");
 const colors = require("colors")
 
 const input = process.argv[2];
 const langCode = franc(input);
 if (langCode === 'und') {
-    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!".red)
+    console.log("Try again with more sample text".red)
 } else {
     const language = langs.where("3", langCode);
-    console.log(`Our best guess is: ${language.name}`.green)
+    console.log(`Detected Language: ${language.name}`.green)
 }
 
