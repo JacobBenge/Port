a26ebc6817943e636bc0181e8f7bb12eea0dcfe3
JacobBenge <benge.jacob@gmail.com>
2021-01-23 23:13:05 -0700

    Exciting Day First Connection to DB

---
+++ WebDev/S38Mongoose&Express/seeds.js
@@ -0,0 +1,65 @@
+const Product = require('./models/product');
+
+const mongoose = require('mongoose');
+const database = "farmStand";
+const port = 27017;
+
+mongoose.connect(`mongodb://localhost:${port}/${database}`, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log(`SUCCESSFULLY CONNECTED TO ${database} DATABASE ON MONGODB SERVER ON PORT: ${port}`)
+    })
+    .catch(err => {
+        console.log(`ERROR IN CONNECTING TO ${database} DATABASE ON MONGODB SERVER ON PORT: ${port}`)
+        console.log(err)
+    })
+
+// const p = new Product({
+//     name: 'Ruby Grapefruit',
+//     price: 1.99,
+//     category: 'fruit'
+
+// })
+
+// p.save()
+//     .then(p => {
+//         console.log(p)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
